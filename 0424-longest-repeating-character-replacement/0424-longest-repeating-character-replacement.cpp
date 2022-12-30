9253333c099439bdcc46c3ca2ec0a75f6f29382a
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2022-12-31 04:12:49 +0530

    Time: 8 ms (90.30%), Space: 7.1 MB (53.89%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int maxf = 0, i = 0, n = s.length();
+        vector<int> count(26);
+        for (int j = 0; j < n; ++j) 
+        {
+            maxf = max(maxf, ++count[s[j] - 'A']);
+            if (j - i + 1 > maxf + k)
+                --count[s[i++] - 'A'];
+        }
+        return n - i;
+    }
+};
