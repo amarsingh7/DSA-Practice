96658a49031e522c7c6731dd28d91055bbad2671
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-01-29 00:06:53 +0530

    Time: 85 ms (47.45%), Space: 33.5 MB (38.43%) - LeetHub

--- 0352-data-stream-as-disjoint-intervals/0352-data-stream-as-disjoint-intervals.cpp
+++ 0352-data-stream-as-disjoint-intervals/0352-data-stream-as-disjoint-intervals.cpp
@@ -1,60 +1,28 @@
 class SummaryRanges {
-public:
-    SummaryRanges() {
-        
-    }
-    
-    void addNum(int value) {
-        auto it = _map.lower_bound(value);
-        bool merged = false;
-        if(it != _map.begin()) {
-            auto prev = it;
-            --prev;
-            if(prev->second + 1 >= value) {
-                merged = true;
-                prev->second = max(prev->second, value);
-            }
-        }
+    set<int> values;
 
-        if(it != _map.end()) {
-            if(it->first - 1 <= value) {
-                if(merged) {
-                    auto prev = it;
-                    --prev;
-                    if(prev->second >= it->first - 1) {
-                        prev->second = max(prev->second, it->second);
-                        _map.erase(it);
-                    }
-                } else {
-                    merged = true;
-                    if(it->first != value) {
-                        pair<int, int> p = *it;
-                        p.first = min(p.first, value);
-                        it = _map.insert(it, p);
-                        ++it;
-                        if(it != _map.end())
-                            _map.erase(it);
-                    }
-                }
+public:
+    SummaryRanges() {}
+
+    void addNum(int value) { values.insert(value); }
+
+    vector<vector<int>> getIntervals() {
+        if (values.empty()) {
+            return {};
+        }
+        vector<vector<int>> intervals;
+        int left = -1, right = -1;
+        for (int value : values) {
+            if (left < 0) {
+                left = right = value;
+            } else if (value == right + 1) {
+                right = value;
+            } else {
+                intervals.push_back({left, right});
+                left = right = value;
             }
         }
-        if(!merged)
-            _map.insert(it, {value, value});
-    }
-    
-    vector<vector<int>> getIntervals() {
-        vector<vector<int>> intervals;
-        for(auto const & p : _map)
-            intervals.push_back({p.first, p.second});
+        intervals.push_back({left, right});
         return intervals;
     }
-
-    map<int, int> _map;
 };
-
-/**
- * Your SummaryRanges object will be instantiated and called as such:
- * SummaryRanges* obj = new SummaryRanges();
- * obj->addNum(value);
- * vector<vector<int>> param_2 = obj->getIntervals();
- */
