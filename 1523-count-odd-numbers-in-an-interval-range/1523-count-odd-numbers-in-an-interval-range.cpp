6b29c3ec7eed0b29184a64b28ed2af3e01cdd7fb
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-02-13 12:50:21 +0530

    Time: 3 ms (66.30%), Space: 6 MB (63.61%) - LeetHub

--- 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cpp
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cpp
@@ -3,13 +3,13 @@ public:
     int countOdds(int low, int high) {
         if(low%2 !=0 && high%2 !=0)
         {
-            return 1+ (high-low)/2; 
+            return 1+ (high-low)/2.0; 
         }
         if(low%2 !=0 && high%2==0 || low%2 ==0 && high%2 !=0)
         {
             return ceil((high-low)/2.0);
         }
         else
-            return (high-low)/2;
+            return (high-low)/2.0;
     }
 };
