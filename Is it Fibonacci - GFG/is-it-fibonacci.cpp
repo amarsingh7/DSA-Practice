df88cf6cad5649190741e92c18c324a465c9ce76
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-02-17 01:56:41 +0530

    Added solution - LeetHub

---
+++ Is it Fibonacci - GFG/is-it-fibonacci.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    long long solve(int N, int K, vector<long long> p) {
+        long long s=0;
+        for(int i=0;i<N;i++)
+        {
+            if(i<K) s+=p[i];
+            else
+            {
+                p.push_back(s); s*=2; s-=p[i-K];
+            }
+        }
+        return p[N-1];
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int N, K;
+        cin >> N >> K;
+
+        vector<long long> GeekNum(K);
+
+        for (int i = 0; i < K; i++) cin >> GeekNum[i];
+
+        Solution ob;
+        cout << ob.solve(N, K, GeekNum) << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
