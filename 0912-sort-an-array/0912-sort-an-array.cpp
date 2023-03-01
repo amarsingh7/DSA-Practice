cce6316b021eda889cdc8d4f845f62624cb74724
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-03-01 16:29:43 +0530

    Time: 245 ms (45.31%), Space: 89 MB (31.63%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,7 +1,29 @@
 class Solution {
 public:
-    vector<int> sortArray(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        return nums;
+    void csort(vector<int> &nums)
+    {
+        unordered_map<int, int> um;
+        int minv= *min_element(nums.begin(), nums.end());
+        int maxv= *max_element(nums.begin(), nums.end());
+        
+        for(auto &i:nums)
+        {
+            um[i]++;
+        }
+            
+        int index=0;
+        for(int i=minv; i<=maxv; i++)
+        {
+            while(um[i]>0)
+            {
+                nums[index]=i;
+                index +=1;
+                um[i] -= 1;
+            }
+        }
+    }
+    vector<int> sortArray(vector<int>& amar) {
+        csort(amar);
+        return amar;
     }
 };
