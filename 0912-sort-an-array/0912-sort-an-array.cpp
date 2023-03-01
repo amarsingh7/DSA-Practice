1a6b979137ec87f95066bf62520f3808cdb52122
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-03-01 17:02:56 +0530

    Time: 92 ms (99.87%), Space: 64.2 MB (42.04%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
-    void csort(vector<int> &nums)
+    vector<int> rsort(vector<int> &nums)
     {
-        unordered_map<int, int> um;
-        int minv= *min_element(nums.begin(), nums.end());
-        int maxv= *max_element(nums.begin(), nums.end());
-        
-        for(auto &i:nums)
-        {
-            um[i]++;
+        int arr[50001]={0};
+        int arr1[50001]={0};
+        for(auto it:nums){
+            if(it<0)arr1[abs(it)]++;
+            else arr[it]++;
         }
-            
-        int index=0;
-        for(int i=minv; i<=maxv; i++)
-        {
-            while(um[i]>0)
-            {
-                nums[index]=i;
-                index +=1;
-                um[i] -= 1;
+        vector<int>ans;
+        for(int i=50000;i>=0;i--){
+            if(arr1[i]>0){
+                for(int j=0;j<arr1[i];j++)ans.push_back(-i);
             }
         }
+        for(int i=0;i<50001;i++){
+            if(arr[i]>0){
+                for(int j=0;j<arr[i];j++)ans.push_back(i);
+            }
+        }
+        return ans;
     }
     vector<int> sortArray(vector<int>& amar) {
-        csort(amar);
-        return amar;
+        return rsort(amar);
     }
 };
