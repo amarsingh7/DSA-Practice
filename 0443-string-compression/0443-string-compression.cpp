2ec28e0be9ca81dfb14d1e6bcd7438d3d8f156ef
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-03-02 20:38:34 +0530

    Time: 10 ms (26.84%), Space: 8.9 MB (83.02%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int n = chars.size();
+    if (n < 2) return n;
+
+    int left = 0, right = 0;
+
+    while (right < n) {
+        chars[left] = chars[right];
+        int count = 0;
+        while (right < n && chars[right] == chars[left]) {
+            count++;
+            right++;
+        }
+
+        if (count == 1) {
+            left++;
+        } else {
+            string cnt = to_string(count);
+            for (auto digit : cnt) {
+                chars[++left] = digit;
+            }
+            left++;
+        }
+    }
+
+    return left;
+    }
+};
