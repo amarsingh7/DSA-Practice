84bf28a46fb728a60238548a6c971b7d8561a88d
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-04-02 00:15:12 +0530

    Time: 16 ms (79.59%), Space: 17.8 MB (50.50%) - LeetHub

--- 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.cpp
+++ 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.cpp
@@ -2,12 +2,63 @@ class Solution {
 public:
     int maximumCount(vector<int>& nums) {
         int n= nums.size();
+        int start=0;
+        int end= n-1;
         
-        int neg= lower_bound(nums.begin(), nums.end(), 0)- nums.begin();
-        int pos= nums.end()- upper_bound(nums.begin(), nums.end(), 0);
-        
-        return max(neg, pos);
-        
-        
+        int ntemp=-1;
+        int ptemp= -1;
+        while(start<=end)
+        {
+
+            int mid= start +(end-start)/2;
+            
+            if(nums[mid]<0)
+            {
+                start= mid+1;
+                ntemp=mid;
+            }
+            else if(nums[mid]>=0)
+            {
+                end= mid-1;
+            }
+            
+        }
+        cout<<ntemp<<" ";
+        start=ntemp+1; end=n-1;
+        while(start<=end)
+        {
+
+            int mid= start +(end-start)/2;
+            
+            if(nums[mid]>0)
+            {
+                 ptemp=mid;
+                end= mid-1;
+
+            }
+            else if(nums[mid]>=0)
+            {
+                start=mid+1;
+            }
+            
+        }
+        if(ntemp==-1){
+            if(ptemp==-1){
+                return 0;
+            }
+            else{
+                return n-ptemp;
+            }
+        }//all pos or 0
+        else{
+          if(ptemp==-1){
+                return ntemp+1;
+            }
+            else{
+                return max(ntemp+1,n-ptemp);
+            }
+        }
+        //  ptemp=n-ptemp;
+        // return max(ptemp,ntemp);
     }
 };
