9a52db74d4c28a13bc5271bc71b90db1517e4184
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-04-04 14:39:10 +0530

    Time: 16 ms (79.12%), Space: 17.7 MB (89.85%) - LeetHub

--- 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.cpp
+++ 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.cpp
@@ -2,63 +2,52 @@ class Solution {
 public:
     int maximumCount(vector<int>& nums) {
         int n= nums.size();
-        int start=0;
+        int start=0; 
         int end= n-1;
         
-        int ntemp=-1;
+        int ntemp= -1;
         int ptemp= -1;
+        
+        //Negative wala case
         while(start<=end)
         {
-
-            int mid= start +(end-start)/2;
-            
+            int mid= start +(end- start)/2;
             if(nums[mid]<0)
             {
                 start= mid+1;
-                ntemp=mid;
+                ntemp= mid;
             }
             else if(nums[mid]>=0)
-            {
                 end= mid-1;
-            }
-            
+                
         }
-        cout<<ntemp<<" ";
-        start=ntemp+1; end=n-1;
+        start= ntemp+1;
+        end= n-1;
+        //Positive wala case
         while(start<=end)
         {
-
             int mid= start +(end-start)/2;
-            
             if(nums[mid]>0)
             {
-                 ptemp=mid;
                 end= mid-1;
-
+                ptemp= mid;
             }
             else if(nums[mid]>=0)
-            {
-                start=mid+1;
-            }
-            
+                start= mid+1;
         }
-        if(ntemp==-1){
-            if(ptemp==-1){
+        //conclusion
+        if(ntemp== -1)
+        {
+            if(ptemp== -1)
                 return 0;
-            }
-            else{
+            else
                 return n-ptemp;
-            }
-        }//all pos or 0
-        else{
-          if(ptemp==-1){
-                return ntemp+1;
-            }
-            else{
-                return max(ntemp+1,n-ptemp);
-            }
         }
-        //  ptemp=n-ptemp;
-        // return max(ptemp,ntemp);
+        else
+        {
+           if(ptemp== -1)
+               return ntemp+1;
+            return max(ntemp+1, n-ptemp);
+        }
     }
 };
