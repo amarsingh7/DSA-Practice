c2f8e76551a76bc8fb9bd45e5dcaf640f213730a
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-04-10 01:28:36 +0530

    Time: 66 ms (73.78%), Space: 14.3 MB (40.37%) - LeetHub

---
+++ 0018-4sum/0018-4sum.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> ans;
+        int n=  nums.size();
+        
+        sort(nums.begin(), nums.end());
+        for(int i=0; i<n; i++)
+        {
+            long long p3= target- nums[i];
+            
+            for(int j=i+1; j<n; j++)
+            {
+               long long p2= p3- nums[j];
+                
+               int front= j+1;
+               int back= n-1;
+               
+                while(front<back)
+                {
+                    int sum2= nums[front]+nums[back];
+                    
+                    if(sum2<p2)
+                       front++;
+                    else if(sum2>p2)
+                        back--;
+                    else
+                    {
+                        ans.push_back({nums[i], nums[j],nums[front], nums[back]});
+                        int lastl= nums[front];
+                        int highh= nums[back];
+                        
+                        while(front<back and nums[front]==lastl)
+                            front++;
+                        while(front<back and nums[back]==highh)
+                            back--;
+                        
+                    }
+                        
+                }
+                if(j+1<n and nums[j+1]==nums[j])
+                    ++j;
+                
+            }
+            
+            if(i+1<n and nums[i+1]==nums[i])
+                ++i;
+        }
+        sort(ans.begin(), ans.end());
+ans.erase(std::unique(ans.begin(), ans.end()), ans.end());
+        
+        return ans;
+    }
+};
