3400a8911e4b2c81625a05416d3283e8c1f55421
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-04-10 02:24:32 +0530

    Time: 386 ms (23.54%), Space: 49.2 MB (34.25%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,49 +1,43 @@
+// #define ll long long;
 class Solution {
 public:
-    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+    vector<vector<int> > fourSum(vector<int> &arr, int k) {
+        // Your code goes here
+        int n = arr.size();
+        sort(arr.begin(), arr.end());
         vector<vector<int>> ans;
-        int n=  nums.size();
-        
-        sort(nums.begin(), nums.end());
-        for(int i=0; i<n; i++)
+        set <vector<int>> s;
+        for (int i=0; i<n; i++)
         {
-            long long p3= target- nums[i];
-            
-            for(int j=i+1; j<n; j++)
+            for (int j=i+1; j<n; j++)
             {
-               long long p2= p3- nums[j];
-                
-               int front= j+1;
-               int back= n-1;
-               
-                while(front<back)
+                int start = j+1;
+                int end = n-1;
+                while (start < end)
                 {
-                    int sum2= nums[front]+nums[back];
-                    
-                    if(sum2<p2)
-                       front++;
-                    else if(sum2>p2)
-                        back--;
-                    else
+                    long long sum;
+                    sum = ((long long)arr[i]+arr[j])+(long long)(arr[start]+arr[end]);
+                    if (sum == k)
                     {
-                        ans.push_back({nums[i], nums[j],nums[front], nums[back]});
-                        int lastl= nums[front];
-                        int highh= nums[back];
-                        
-                        while(front<back and nums[front]==lastl)
-                            front++;
-                        while(front<back and nums[back]==highh)
-                            back--;
-                        
+                        s.insert({arr[i],arr[j],arr[start],arr[end]});
+                        start++;
+                        end--;
                     }
-                        
+                    else if (sum > k)
+                    {
+                        end--;
+                    }
+                    else 
+                    {
+                        start++;
+                    }
                 }
             }
-            
         }
-        sort(ans.begin(), ans.end());
-ans.erase(std::unique(ans.begin(), ans.end()), ans.end());
-        
+        for(auto i:s)
+        {
+            ans.push_back(i);
+        }
         return ans;
     }
 };
