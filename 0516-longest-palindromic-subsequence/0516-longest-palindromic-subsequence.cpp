da768bd8040f13a0b243cd693724c301b749ae5b
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-04-15 02:01:28 +0530

    Time: 149 ms (52.28%), Space: 73.3 MB (5.03%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -23,20 +23,5 @@ public:
         vector<vector<int>> dp(m+1, vector<int>(m+1, -1));
         return lcs(s, r, n, m, dp);
         
-//         vector<int> dp(n), dpp(n);
-        
-//         for(int i=n-1; i>=0; i--)
-//         {
-//             dp[i]=1;
-//             for(int j=i+1; j<n; j++)
-//             {
-//                 if(s[i]==s[j])
-//                     dp[j]= dpp[j-1] +2;
-//                 else
-//                     dp[j]= max(dpp[j], dp[j-1]);
-//             }
-//             dpp= dp;
-//         }
-//         return dp[n-1];
     }
 };
