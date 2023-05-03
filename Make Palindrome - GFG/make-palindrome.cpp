e17ac2e83becee688aebb9e6c557da90aef714e1
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-05-03 16:00:16 +0530

    Added solution - LeetHub

---
+++ Make Palindrome - GFG/make-palindrome.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    bool makePalindrome(int n,vector<string> &arr){
+        // Code here
+        unordered_map<string, int> mp;
+        for(int i=0; i<n; i++)
+        {
+            mp[arr[i]]++;
+        }
+        
+        for(int i=0; i<n;i++)
+        {
+            string s= arr[i];
+            string p= string(s.rbegin(), s.rend());
+            if(mp[s] !=mp[p])
+            {
+                if(s !=p)
+                    return false;
+            }
+        }
+        return true;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<string> arr(n);
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        Solution ob;
+        if(ob.makePalindrome(n,arr)) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
