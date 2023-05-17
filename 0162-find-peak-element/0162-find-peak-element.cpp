c40adf0b6f4e0f4a0af21c14692ce299a3be998b
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-05-17 18:35:36 +0530

    Time: 6 ms (32.28%), Space: 8.8 MB (97.01%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -3,6 +3,18 @@ public:
     int findPeakElement(vector<int>& arr) {
         int n= arr.size();
         
-        return max_element(arr.begin(), arr.end())- arr.begin();
+        int start=0;
+        int end= n-1;
+        while(start<end-1)
+        {
+            int mid= (start+end) /2;
+            if(arr[mid]>arr[mid+1] and arr[mid] >arr[mid-1])
+                return mid;
+            else if(arr[mid]>arr[mid+1])
+                end= mid-1;
+            else
+                start= mid+1;
+        }
+        return arr[start]>arr[end] ? start:end;
     }
 };
