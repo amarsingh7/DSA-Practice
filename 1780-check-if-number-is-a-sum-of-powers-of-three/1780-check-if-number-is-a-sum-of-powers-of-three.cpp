e11ebb6d79981ee45e746d8f9f824a5403634c62
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-05-27 20:05:29 +0530

    Time: 0 ms (100.00%), Space: 6 MB (57.87%) - LeetHub

--- 1780-check-if-number-is-a-sum-of-powers-of-three/1780-check-if-number-is-a-sum-of-powers-of-three.cpp
+++ 1780-check-if-number-is-a-sum-of-powers-of-three/1780-check-if-number-is-a-sum-of-powers-of-three.cpp
@@ -3,12 +3,13 @@ public:
     
     
     bool checkPowersOfThree(int n) {
-        while(n>0)
-        {
-            if(n%3 ==2)
-                return false;
-            n/=3;
-        }
-        return true;
+        if(n==1)
+            return true;
+        if(n%3==0)
+            return checkPowersOfThree(n/3);
+        if(n%3==1)
+            return checkPowersOfThree((n-1)/3);
+        return false;
+        
     }
 };
