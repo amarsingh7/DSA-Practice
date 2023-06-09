146f3047fbfc5977d431e5f5cbbfe0de0768ad44
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-06-10 01:08:20 +0530

    Time: 19 ms (41.95%), Space: 15.9 MB (72.41%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
-        int n= letters.size();
-        char ans;
-        for(int i=0; i<n; i++)
-        {
-            if(letters[i]>target)
-            {
-                ans= letters[i];
-                break;
-            }
-        }
-        if(ans>target)
-            return ans;
-        return letters[0];
+        // int n= letters.size();
+        // char ans;
+        // for(int i=0; i<n; i++)
+        // {
+        //     if(letters[i]>target)
+        //     {
+        //         ans= letters[i];
+        //         break;
+        //     }
+        // }
+        // if(ans>target)
+        //     return ans;
+        // return letters[0];
+        auto it = upper_bound(letters.begin(), letters.end(), target);
+        return it == letters.end() ? letters[0] : *it;
     }
 };
