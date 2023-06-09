4bc9ccbbf7dc7c116d8bac08ecd7c1670a70cec7
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-06-10 01:30:44 +0530

    Time: 4 ms (72.53%), Space: 9 MB (97.80%) - LeetHub

--- 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.cpp
+++ 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.cpp
@@ -2,17 +2,15 @@ class Solution {
 public:
     int countElements(vector<int>& nums) {
         bool ans= false;
+        int n= nums.size();
         int firstElement = nums[0];
-        for (int i = 1; i < nums.size(); i++) 
+        for (int i = 1; i < n; i++) 
         {
             if (nums[i] != firstElement) 
-            ans=true; // Found a different element, not all elements are the same
-        
+            ans=true; 
         }
         if(ans==false)
             return 0;
-        int n= nums.size();
-        sort(nums.begin(), nums.end());
         int minval= *min_element(nums.begin(), nums.end());
         int maxval= *max_element(nums.begin(), nums.end());
     
