3b11dc66145876e5592ea9fdf09a29012e268e46
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-06-10 01:47:27 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (87.00%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int fac(int n)
+    {
+        if(n==1)
+            return 1;
+        return n*fac(n-1);
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int n=fac(nums.size());
+        
+        while(n--)
+        {
+            ans.push_back(nums);
+            next_permutation(nums.begin(), nums.end());
+        }
+        return ans;
+       
+    }
+};
