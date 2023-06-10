3a35d6f4615a54f2ff6bf6412bd7e8319389238d
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-06-10 11:05:43 +0530

    Time: 8 ms (36.18%), Space: 10 MB (99.26%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -2,24 +2,13 @@ class Solution {
 public:
     string findDifferentBinaryString(vector<string>& nums) {
        
-        int n= nums[0].size();
-        vector<string> vec;
-        for(int i=0; i<pow(2, n); i++)
-        {
-            std::bitset<16> binary(i);
-            string res= binary.to_string();
-            vec.push_back(res.substr(16-n, n));
-        }
-        string ans="";
-        for(int i=0; vec.size(); i++)
-        {
-            if(find(nums.begin(), nums.end(), vec[i])== nums.end())
-            {
-                ans+= vec[i];
-                break;
-            }
-        }
-        
+       int n= nums.size();
+       string ans="";
+       for(int i=0; i<n; i++)
+       {
+           ans+= nums[i][i]=='1'? '0':'1';
+           /* nums.length and the nums[i].length are sam eso thats why it worked. */
+       }
         return ans;
     }
 };
