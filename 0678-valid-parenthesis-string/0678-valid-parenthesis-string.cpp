97ad8fa3d78e12d4591321cf8f800c35f451efe1
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-06-12 00:07:19 +0530

    Time: 4 ms (38.11%), Space: 6.1 MB (87.57%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     bool checkValidString(string s) {
-       int lo = 0, hi = 0;
+       int op = 0, cl = 0;
        for (char c: s) 
        {
            if(c == '(')
-                lo += 1;
+                op += 1;
            else
-               lo -= 1;
+               op -= 1;
            if(c != ')')
-               hi += 1;
+               cl += 1;
            else
-               hi -= 1;
-           if (hi < 0) 
+               cl -= 1;
+           if (cl < 0) 
                 break;
-           lo = max(lo, 0);
+           op = max(op, 0);
        }
-       return lo == 0;
+       return op == 0;
     }
 };
