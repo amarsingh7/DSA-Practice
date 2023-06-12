26f76514db4006dfdb0db8c71dc01e4343fa8b82
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-06-12 21:12:22 +0530

    Added solution - LeetHub

---
+++ Rod Cutting - GFG/rod-cutting.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+  public:
+  int ans(int prices[], int n)
+  {
+    vector<int> dp(n+1, 0);
+    for(int i=1; i<=n; i++)
+    {
+        for(int j=1; j<=i; j++)
+            dp[i]= max(dp[i], prices[j-1]+dp[i-j]);
+    }
+    return dp[n];
+  }
+    int cutRod(int prices[], int n) {
+        //code here
+        return ans(prices, n);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++) 
+            cin >> a[i];
+            
+        Solution ob;
+
+        cout << ob.cutRod(a, n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
