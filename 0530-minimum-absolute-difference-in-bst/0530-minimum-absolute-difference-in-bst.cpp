cdf5772f1c27e7c59e71e65d0fd70b1d43c0465b
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-06-14 21:57:05 +0530

    Time: 15 ms (87.59%), Space: 25.2 MB (76.48%) - LeetHub

--- 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.cpp
+++ 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.cpp
@@ -14,10 +14,13 @@ public:
         int res = INT_MAX, pre = -1;
     int getMinimumDifference(TreeNode* root) 
     {
-        if (root->left != NULL) getMinimumDifference(root->left);
-        if (pre >= 0) res = min(res, root->val - pre);
+        if (root->left != NULL) 
+            getMinimumDifference(root->left);
+        if (pre >= 0) 
+            res = min(res, root->val - pre);
         pre = root->val;
-        if (root->right != NULL) getMinimumDifference(root->right);
+        if (root->right != NULL) 
+            getMinimumDifference(root->right);
         return res;
     }
 };
