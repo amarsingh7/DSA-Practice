e8ffa019956a1e05a0c1d5033744ac7a4f49cdf3
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-06-24 18:08:34 +0530

    Time: 81 ms (86.03%), Space: 9.2 MB (35.67%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -6,17 +6,15 @@ public:
     vector<vector<bool>> isPalindrome(n, vector<bool>(n, false)); 
         // to store palindrome information
 
-    for (int i = 0; i < n; i++) 
-    {
-        for (int j = 0; j <= i; j++) 
-        {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= i; j++) {
             if (s[i] == s[j] && (i - j <= 1 || isPalindrome[j + 1][i - 1]))             {
                 isPalindrome[j][i] = true; 
                 // mark substring s[j..i] as palindrome
-                if (j == 0) 
+                if (j == 0)
                     dp[i] = 0; // no cuts needed for the substring s[0..i]
-                else
-                    dp[i] = min(dp[i], dp[j - 1] + 1);
+                else 
+                    dp[i] = min(dp[i], dp[j - 1] + 1); 
                 // update the minimum cuts
             }
         }
