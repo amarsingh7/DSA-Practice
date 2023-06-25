c5a7af8cf9c1ec406bc2ea33f1b4c9ba860cc348
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-06-25 18:59:29 +0530

    Time: 297 ms (34.20%), Space: 45.8 MB (57.14%) - LeetHub

--- 0956-tallest-billboard/0956-tallest-billboard.cpp
+++ 0956-tallest-billboard/0956-tallest-billboard.cpp
@@ -1,36 +1,40 @@
 class Solution {
 public:
+    
+    /* 
+    DP Appproch     time- O(n*sum)
+    Just like 0-1 knapsack where at each iteration we had choice to                   take/leave, here we have 3 choice:
+    - Include in l1
+    - Include in l2
+    - Leave it
+    * dp[i][j] = Max sum of group l1 considering the first i rods such that the         difference between the sum of l1 and l2 is (sum of all rods - j)
+
+    */
+    
     int tallestBillboard(vector<int>& rods) {
-        const auto sum = reduce(rods.begin(), rods.end());
-        // Difference cannot be larger than half of the sum.
-        vector memo(rods.size(), vector(sum / 2 + 1, -1));
-        return helper(rods, 0, sum / 2, 0, memo);
+        int n= rods.size();
+        int sum= accumulate(rods.begin(), rods.end(), 0);
+        int limit= 2*sum;
+        vector<vector<int>> dp(n+1, vector<int>(limit, INT_MIN));
+        dp[0][sum]=0;
+        
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=0; j<limit; j++)
+            {
+                // putting in l1
+                if((j- rods[i-1]) >= 0 && dp[i-1][j - rods[i-1]] != INT_MIN)
+                    dp[i][j] = max(dp[i][j], dp[i-1][j - rods[i-1]] + rods[i-1]);
+                // Putting ith in l2
+                if ((j+ rods[i-1]) < limit && dp[i-1][j + rods[i-1]] != INT_MIN)
+                    dp[i][j] = max(dp[i][j], dp[i-1][j + rods[i-1]]);
+                // Ignoring ith rod
+                if (dp[i-1][j] != INT_MIN)
+                    dp[i][j] = max(dp[i][j], dp[i-1][j]);
+            }
+        }
+        return dp[n][sum];
     }
+    
 
-    int helper(
-        const vector<int>& rods,
-        const size_t idx,
-        const int limit,
-        const int diff,
-        vector<vector<int>>& memo) {
-
-        if (idx == rods.size())
-            return diff == 0 ? 0 : INT_MIN; 
-        // Using INT_MIN as a sentinel value works because the sum cannot be larger than 5000.
-
-        if (diff > limit)
-            return INT_MIN;
-
-        auto& m = memo[idx][diff];
-        if (m != -1)
-            return m;
-
-        return m = max({
-            // Length does not change when we don't pick the rod.
-            helper(rods, idx + 1, limit, diff, memo),
-            // Length increases when we add the rod to the longer side.
-            helper(rods, idx + 1, limit, diff + rods[idx], memo) + rods[idx],
-            // Length increases depending on whether the rod is longer than the difference between the sides.
-            helper(rods, idx + 1, limit, abs(diff - rods[idx]), memo) + (diff > rods[idx] ? 0 : rods[idx] - diff)});
-    }
 };
