5eb6e3389260dc120faa9088c9ed6f1cc1a10527
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-07-02 01:55:36 +0530

    Time: 920 ms (66.35%), Space: 7 MB (10.69%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     int ans= INT_MAX;
-    void dfs(vector<int> &cookies, vector<int> &child, int idk, int maxi)
+    void dfs(vector<int> &cookies, vector<int> &child, int i, int maxi)
     {
-        if(idk>= cookies.size())
+        if(i>= cookies.size())
         {
             ans= min(ans, maxi);
             return;
         }    
-        int c= cookies[idk];
-        for(int i=0; i<child.size(); i++)
+        int c= cookies[i];
+        for(int j=0; j<child.size(); j++)
         {
-            child[i]+= c;
-            dfs(cookies, child, idk+1, max(maxi, child[i]));
-            child[i]-= c;
+            child[j]+= c;
+            dfs(cookies, child, i+1, max(maxi, child[j]));
+            child[j]-= c;
         }
             
     }
     int distributeCookies(vector<int>& cookies, int k) {
-        vector<int> dist(k, 0);
-        dfs(cookies, dist, 0, 0);
+        vector<int> child(k, 0);
+        dfs(cookies, child, 0, 0);
         return ans;
     }
 };
