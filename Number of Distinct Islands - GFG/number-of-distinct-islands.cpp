506ca32d8f0edf775185ea129e39c6cd88fb20e9
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-07-02 18:06:09 +0530

    Added solution - LeetHub

--- Number of Distinct Islands - GFG/number-of-distinct-islands.cpp
+++ Number of Distinct Islands - GFG/number-of-distinct-islands.cpp
@@ -9,46 +9,37 @@ using namespace std;
 
 class Solution {
   public:
-    void dfs(int r,int c,int m,int n,vector<vector<int>>&vis,vector<vector<int>>&grid,vector<pair<int,int>>&v)
-        {
-        if(r<0 || c<0 || r>=m ||c>=n || vis[r][c] || grid[r][c]==0){
-           return;
-        }
-        v.push_back({r,c});
-        vis[r][c]=1;
-        dfs(r+1,c,m,n,vis,grid,v);
-        dfs(r-1,c,m,n,vis,grid,v);
-        dfs(r,c+1,m,n,vis,grid,v);
-        dfs(r,c-1,m,n,vis,grid,v);
+    void dfs(int r,int c,int ro,int co,vector<vector<int>>&grid,vector<pair<int,int>>&v)
+    {
+            int m= grid.size();
+            int n= grid[0].size();
+            if(r<0 || c<0 || r>=m ||c>=n ||grid[r][c]<=0)
+                return;
+        v.push_back({r-ro, c-co});
+        grid[r][c] *= -1;
+        dfs(r+1, c, ro, co, grid, v);
+        dfs(r-1, c, ro, co, grid, v);
+        dfs(r, c+1, ro, co, grid, v);
+        dfs(r, c-1, ro, co, grid, v);
         
    }
     int countDistinctIslands(vector<vector<int>>& grid) {
         // code here
-        int  count=0;
         int m=grid.size();
         int n=grid[0].size();
-        vector<vector<int>>vis(m,vector<int>(n,0));
+        if(m==0 || n==0)
+            return 0;
+            
         set<vector<pair<int,int>>>s;
-        for(int r=0;r<m;r++)
+        for(int r=0; r<m; r++)
         {
-            for(int c=0;c<n;c++)
+            for(int c=0; c<n; c++)
             {
-                if(!vis[r][c] && grid[r][c]==1)
-                {
-                    vector<pair<int,int>> v;
-                    dfs(r,c,m,n,vis,grid,v);
-                    auto t=v[0];
-                    for(auto &it:v)
-                    {
-                        it.first-=t.first;
-                        it.second-=t.second;
-                    }
-                    if(s.find(v)==s.end())
-                    {
-                        s.insert(v);
-                    }
-                   
-                }
+                if(grid[r][c] != 1)
+                    continue;
+                vector<pair<int, int>> v;
+                dfs(r, c, r, c, grid, v);
+                s.insert(v);
             }
         }
         return s.size();
