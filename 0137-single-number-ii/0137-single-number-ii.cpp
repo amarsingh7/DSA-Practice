83609ddecca3c3b177ab3f032207c14b886f0e46
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-07-05 00:00:09 +0530

    Time: 2 ms (98.56%), Space: 9.5 MB (72.62%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,20 +1,13 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int s= nums.size();
-        unsigned int shift = 1;
-        unsigned int ans = 0;
-        
-        for (int i = 0; i < 32; ++i) {
-            int count = 0;
-            for (int i : nums) 
-                if (i & shift)
-                    count += 1;
-            if ((count % 3) != 0)
-                ans = ans + shift;
-            shift <<= 1;
+        int seenOnce = 0, seenTwice = 0;
+
+        for (int num: nums) {
+            seenOnce = (seenOnce ^ num) & (~seenTwice);
+            seenTwice = (seenTwice ^ num) & (~seenOnce);
         }
-        
-        return ans;
+
+        return seenOnce;
     }
 };
