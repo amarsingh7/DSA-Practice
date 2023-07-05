03b8af1ff245df4b4246c22b88b05c2f8347df76
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-07-06 00:52:14 +0530

    Time: 39 ms (86.38%), Space: 36.4 MB (65.98%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -4,10 +4,11 @@ public:
         int k=1;
         int i=0;
         int start;
-        
-        for(start=0; start<nums.size(); start++)
+        int n= nums.size();
+        for(start=0; start<n; start++)
         {
-            if(nums[start]==0) k--;
+            if(nums[start]==0) 
+                k--;
             if(k<0 and nums[i++]==0)
                 k++;
         }
