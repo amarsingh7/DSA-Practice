c6cdbb1adeb40abbd5b198f9a45da2342017bc15
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-07-07 17:02:03 +0530

    Time: 1104 ms (5.05%), Space: 6.8 MB (63.84%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -2,11 +2,12 @@ class Solution {
 public:
     bool isMatch(string s, string p) {
         if (s == p)
-            return true;
-        return isMatch(0, 0, s, p);
+            return true; 
+        vector<vector<char>> memo(s.size() +1, vector<char>(p.size(), -1));
+        return isMatch(0, 0, s, p, memo);
     }
 
-    bool isMatch(int i, int j, string& s, string& p) {
+    bool isMatch(int i, int j, string& s, string& p, vector< vector<char>> &memo) {
         int sn = s.size();
         int pn = p.size();
 
@@ -14,16 +15,16 @@ public:
             return i == sn;
 
         if (j + 1 < pn && p[j + 1] == '*') {
-            if (isMatch(i, j + 2, s, p))
+            if (isMatch(i, j + 2, s, p, memo))
                 return true;
             if (i < sn && (s[i] == p[j] || p[j] == '.')) {
-                return isMatch(i + 1, j, s, p);
+                return memo[i][j]=isMatch(i + 1, j, s, p, memo);
             }
         }
         
         if (i < sn && (p[j] == '.' || s[i] == p[j]))
-            return isMatch(i + 1, j + 1, s, p);
+            return memo[i][j]=isMatch(i + 1, j + 1, s, p, memo);
 
-        return false;
+        return memo[i][j]=0;
     }
 };
