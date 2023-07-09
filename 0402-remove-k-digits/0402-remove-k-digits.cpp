547155fc4304b8497fe2d187f5d47f6c95562e84
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-07-10 02:41:40 +0530

    Time: 13 ms (70.53%), Space: 8.7 MB (68.17%) - LeetHub

---
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+    stack<char> st;
+    int n = num.size();
+    
+    if (n <= k)   
+        return "0";
+    
+    if (k == 0)
+        return num;
+    
+    st.push(num[0]);
+
+    for (int i = 1; i < n; i++) {
+        while (k > 0 && !st.empty() && num[i] < st.top()) {
+            st.pop();
+            k--;
+        }
+        st.push(num[i]);
+        
+        if (st.size() == 1 && st.top() == '0')
+            st.pop();
+    }
+  
+    while (k > 0 && !st.empty()) {
+        st.pop();
+        k--;
+    }
+  
+    string ans;
+    while (!st.empty()) {
+        ans.push_back(st.top());
+        st.pop();
+    }
+    
+    reverse(ans.begin(), ans.end());
+  
+    if (ans.empty())
+        return "0";
+    
+    return ans;
+
+    }
+};
