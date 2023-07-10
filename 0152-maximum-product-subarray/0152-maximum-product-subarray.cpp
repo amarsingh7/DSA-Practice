114f17a938be5893f9b4377922ec6d7b2fb0fa5c
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-07-10 23:25:36 +0530

    Time: 8 ms (59.39%), Space: 13.7 MB (62.74%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -4,10 +4,10 @@ public:
         int maxprod=nums[0];
         int curmin= nums[0];
         int curmax= nums[0];
-        
+         
         for(int i=1; i<nums.size(); i++)
         {
-            if(nums[i]< 0)
+            if(nums[i]<0)
                 swap(curmin, curmax);
             curmin= min(nums[i], curmin*nums[i]);
             curmax= max(nums[i], curmax*nums[i]);
