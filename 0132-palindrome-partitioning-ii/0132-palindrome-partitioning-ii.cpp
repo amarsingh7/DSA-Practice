fe847830b52d08f2b81837996f381689f5321c3d
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-07-13 19:38:48 +0530

    Time: 64 ms (93.15%), Space: 10.8 MB (31.67%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -3,13 +3,17 @@ public:
     int minCut(string s) {
     int n = s.size();
     vector<int> dp(n, INT_MAX); 
-    vector<vector<bool>> isPalindrome(n, vector<bool>(n, false)); 
+    bool dpans[n][n];
+    memset(dpans, false, sizeof(dpans));
         // to store palindrome information
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j <= i; j++) {
-            if (s[i] == s[j] && (i - j <= 1 || isPalindrome[j + 1][i - 1]))             {
-                isPalindrome[j][i] = true; 
+    for (int i = 0; i < n; i++) 
+    {
+        for (int j = 0; j <= i; j++) 
+        {
+            if (s[i] == s[j] && (i - j <= 1 || dpans[j + 1][i - 1]))             
+            {
+                dpans[j][i] = true; 
                 // mark substring s[j..i] as palindrome
                 if (j == 0)
                     dp[i] = 0; // no cuts needed for the substring s[0..i]
