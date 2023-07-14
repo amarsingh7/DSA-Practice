265f238089e44aa07394034cef03aa8f4fbb7966
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-07-14 14:45:53 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (97.57%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
-    int fac(int n)
-    {
-        if(n==1 ||n==0)
-            return 1;
-        return n*fac(n-1);
-    }
+    // int fac(int n)
+    // {
+    //     if(n==1 ||n==0)
+    //         return 1;
+    //     return n*fac(n-1);
+    // }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        int n=fac(nums.size());
+        int n= nums.size();
+        vector<int> fac(n+1);
+        fac[0]= 1;
+        for(int i=1; i<=n; i++)
+            fac[i]= fac[i-1]*i;
         
-        while(n--)
+        while(fac[n]--)
         {
             ans.push_back(nums);
             next_permutation(nums.begin(), nums.end());
