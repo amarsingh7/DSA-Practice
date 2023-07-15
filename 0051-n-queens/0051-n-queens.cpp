584c9b0344883f079a9129678489ed6b79a26cbe
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-07-15 21:41:24 +0530

    Time: 13 ms (35.25%), Space: 7.3 MB (55.01%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -7,17 +7,17 @@ public:
         for(int i=row; i>=0; i--)
             if(board[i][col]=='Q')  // complete col
                 return false;
-        for(int i=row, j=col; i>=0 and j>=0; i--,j--)  // left diagonal
-            if(board[i][j]=='Q')
+        for(int i=row, j=col; i>=0 and j>=0; i--,j--)  
+            if(board[i][j]=='Q')    // left diagonal
                 return false;
         for(int i=row, j=col; i>=0 and j<board.size(); i--, j++)
-            if(board[i][j]=='Q')
+            if(board[i][j]=='Q')    // right diagonal
                 return false;
         return true;
     }
     void dfs(vector<string> &board, int row)
     {
-        if(row== board.size())
+        if(row== board.size())     // exploration done
         {
             ans.push_back(board);
             return;
@@ -27,9 +27,9 @@ public:
         {
             if(isvalid(board, row, i))
             {
-                board[row][i]= 'Q';
-                dfs(board, row+1);
-                board[row][i]= '.';
+                board[row][i]= 'Q';     // safe positon
+                dfs(board, row+1);      // move to next row
+                board[row][i]= '.';     // backtrack
             }
         }
     }
