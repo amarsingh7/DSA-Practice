af305276520c222cf78d458920666e4d371aa19b
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-07-15 21:47:08 +0530

    Time: 7 ms (53.37%), Space: 6.6 MB (39.40%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    
+    vector<vector<string >> ans;
+        
+    bool isvalid(vector<string> &board, int row, int col)
+    {
+        for(int i=row; i>=0; i--)
+            if(board[i][col]=='Q')  // complete col
+                return false;
+        for(int i=row, j=col; i>=0 and j>=0; i--,j--)  
+            if(board[i][j]=='Q')    // left diagonal
+                return false;
+        for(int i=row, j=col; i>=0 and j<board.size(); i--, j++)
+            if(board[i][j]=='Q')    // right diagonal
+                return false;
+        return true;
+    }
+    void dfs(vector<string> &board, int row)
+    {
+        if(row== board.size())     // exploration done
+        {
+            ans.push_back(board);
+            return;
+        }
+            
+        for(int i=0; i<board.size(); i++)
+        {
+            if(isvalid(board, row, i))
+            {
+                board[row][i]= 'Q';     // safe positon
+                dfs(board, row+1);      // move to next row
+                board[row][i]= '.';     // backtrack
+            }
+        }
+    }
+    
+    int totalNQueens(int n) {
+        vector<string> board(n, string(n, '.'));
+        dfs(board, 0);
+        
+        return ans.size();
+    }
+};
