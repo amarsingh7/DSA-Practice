a9544a11a65c7476c4506b56de547f11e7091854
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-07-17 20:55:24 +0530

    Time: 23 ms (70.61%), Space: 18.3 MB (58.37%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -32,24 +32,21 @@ public:
     }
 
     // Check subgrids
-    for (int k = 0; k < 9; ++k) {
-        vector<bool> subgridCheck(9, false);
-        int startRow = (k / 3) * 3;
-        int startCol = (k % 3) * 3;
-        for (int i = startRow; i < startRow + 3; ++i) {
-            for (int j = startCol; j < startCol + 3; ++j) {
-                char current = board[i][j];
-                if (current != '.') {
-                    int num = current - '1';
-                    if (subgridCheck[num]) {
-                        return false;
+    
+    for (int row = 0; row < 9; row += 3) {
+        for (int col = 0; col < 9; col += 3) {
+            vector<bool> subgridCheck(9, false);
+            for (int i = 0; i < 9; ++i) {
+                    char current = board[3*(row/3) + i/3][3*(col/3) + i%3];
+                    if (current != '.') {
+                        if (subgridCheck[current-'1']) 
+                            return false;
+                        subgridCheck[current-'1'] = true;
                     }
-                    subgridCheck[num] = true;
-                }
             }
         }
-    }
-
+    }    
+        
     return true;
 }
 
