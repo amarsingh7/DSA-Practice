b211d36db182b8da8fb045cf59bac5428ca7a85e
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-07-19 14:24:57 +0530

    Time: 122 ms (65.79%), Space: 67.7 MB (34.77%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,21 +1,8 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& arr) {
-        // int cbest=INT_MIN, obest=0;
-        // if(nums.size() ==1)
-        //     return nums[0];
-        // for( int it=0; it< nums.size(); it++)
-        // {
-        //     if(cbest >= 0)
-        //         cbest += nums[it];
-        //     else
-        //         cbest = nums[it];
-        //     if(cbest > obest)
-        //         obest = cbest;
-        // }
-        // return obest;
-         int msf=arr[0], sum=0;
-        int n= arr.size();
+       int msf=arr[0], sum=0;
+       int n= arr.size();
        for(int i=0;i<n;i++) //maximizing sum
        {
            sum+= arr[i];
