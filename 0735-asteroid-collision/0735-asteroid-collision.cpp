cff421e6b346e5a4def744a7185016425a4bfcca
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-07-23 19:23:15 +0530

    Time: 11 ms (92.27%), Space: 17.8 MB (50.42%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,16 +1,31 @@
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
-    vector<int> res;
-	for(int a : asteroids) 
-    {
-		bool destroyed = false;
-		while(res.size() && res.back() > 0 && a < 0 && !destroyed) {
-			if(res.back() >= -a) destroyed = true;
-			if(res.back() <= -a) res.pop_back();
-		}
-		if(!destroyed) res.push_back(a);
-	}
-	return res;
+        stack<int> st;
+        int n= asteroids.size();
+        
+        for(auto ast: asteroids)
+        {
+           int flag= 1;
+           while(!st.empty() and (st.top() >0 and ast <0))
+           {
+               if(abs(st.top()) < abs(ast)){
+                   st.pop(); continue;
+               }
+                   
+               if(abs(st.top()) == abs(ast))
+                   st.pop();
+               flag=0;
+               break;
+           }
+           if(flag)
+               st.push(ast);
+        }
+        vector<int> ans;
+        while(!st.empty())
+            ans.push_back(st.top()), st.pop();
+        reverse(ans.begin(), ans.end());
+        
+        return ans;
     }
 };
