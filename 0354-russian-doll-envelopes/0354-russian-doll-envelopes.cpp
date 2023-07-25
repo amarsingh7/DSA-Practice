6f287ac7fd8e5f77a43956099737c124f2dac6b6
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-07-26 01:16:05 +0530

    Time: 382 ms (90.05%), Space: 77.4 MB (76.48%) - LeetHub

--- 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
@@ -5,7 +5,7 @@ public:
     if (n == 0) return 0;
 
     // Sort envelopes based on width in ascending order.
-    sort(envelopes.begin(), envelopes.end(), [](const vector<int>& a, const vector<int>& b) {
+    sort(envelopes.begin(), envelopes.end(), [](vector<int>& a, vector<int>& b) {
         return a[0] < b[0] || (a[0] == b[0] && a[1] > b[1]); 
         // If width is same, sort by height in descending order.
     });
