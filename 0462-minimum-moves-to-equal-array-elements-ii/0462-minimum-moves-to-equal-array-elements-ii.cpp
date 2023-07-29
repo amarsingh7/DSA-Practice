6f0606afcadb7b868aba6e73115218db8236e61a
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-07-29 14:16:48 +0530

    Time: 9 ms (93.39%), Space: 11.7 MB (86.37%) - LeetHub

---
+++ 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minMoves2(vector<int>& nums) {
+        int n= nums.size();
+        sort(nums.begin(), nums.end());
+        if(n==1)
+            return 0;
+        if(n==2)
+            return abs(nums[0]-nums[1]);
+        int avg=0;
+        if(n%2 !=0)
+            avg= nums[(n-1)/2];
+        if(n%2 ==0)
+            avg= (nums[n/2]+ nums[(n-2)/2])/2;
+        for(int i=0; i<n; i++)
+        {
+            nums[i]= abs(nums[i]- avg);
+        }
+        return accumulate(nums.begin(), nums.end(), 0L);
+    }
+};
