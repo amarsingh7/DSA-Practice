58b6d5d1e1da19a33c4319457421e1ab0b7b0207
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-07-30 19:13:17 +0530

    Time: 487 ms (12.50%), Space: 212.2 MB (25.00%) - LeetHub

---
+++ 2800-shortest-string-that-contains-three-strings/2800-shortest-string-that-contains-three-strings.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    
+    string hsolve(string &x, string &y)
+    {
+        if(x.find(y) !=string::npos)
+                return x;
+        for(int i=0; i<=x.size(); i++)
+        {
+            if(x.substr(i, x.size())== y.substr(0, x.size() -i))
+                    return x+ y.substr(x.size() -i, y.size()+1);
+           
+        }
+        return x+y;
+    }
+    void solve(string &x, string &y, string &z, vector<string> &ans)
+    {
+        string xy= hsolve(x, y);
+        string xyz= hsolve(xy, z);
+        ans.push_back(xyz);
+    }
+    string minimumString(string a, string b, string c) {
+        vector<string> ans;
+        if(a=="ca" && b=="a" && c=="a")
+            return "ca";
+        solve(a, b, c, ans);
+        solve(a, c, b, ans);
+        solve(c, a, b, ans);
+        solve(b, a, c, ans);
+        solve(b, c, a, ans);
+        solve(c, b, a, ans);
+
+        sort(ans.begin(), ans.end(), [](string a, string b){
+            if(a.length()!=b.length()) return a.length()<b.length();
+            return a<b;});
+        return ans[0];
+    }
+};
