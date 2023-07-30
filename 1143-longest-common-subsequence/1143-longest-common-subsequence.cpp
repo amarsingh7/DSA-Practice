172f7ed6048a4112952d9d439860f4fe29c5c463
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-07-30 22:33:11 +0530

    Time: 22 ms (99.62%), Space: 10.6 MB (81.26%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,23 +1,12 @@
 class Solution {
 public:
-    // int lcs(string &s, string &r, int m, int n, vector<vector<int>> &dp)
-    // {
-    //     if(m==0 || n==0)
-    //         return 0;
-    //     if(s[m-1]==r[n-1])
-    //         //return 1+lcs(s, r, m-1, n-1);
-    //         return dp[m][n]= 1+ dp[m-1][n-1];
-    //     if(dp[m][n] != -1)
-    //         return dp[m][n];
-    //     else
-    //         return dp[m][n]= max(dp[m-1][n], dp[m][n-1]);
-    // }
+   
+    int dp[1001][1001];
     int longestCommonSubsequence(string s, string r) {
         int m= s.size();
         int n= r.size();
         
-        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
-        
+        memset(dp, 0, sizeof(dp));
         for(int i=1; i<=m; i++)
         {
             for(int j=1; j<=n; j++)
