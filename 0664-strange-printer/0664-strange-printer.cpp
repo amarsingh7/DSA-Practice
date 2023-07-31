7629f82d61b70a6326f99117a20cad061a21fd68
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-07-31 15:50:22 +0530

    Time: 12 ms (98.88%), Space: 6.1 MB (79.63%) - LeetHub

--- 0664-strange-printer/0664-strange-printer.cpp
+++ 0664-strange-printer/0664-strange-printer.cpp
@@ -5,21 +5,21 @@ public:
         
         s.erase(unique(s.begin(), s.end()), s.end());
         int n = s.size();
-        for (int i = 0; i < n; ++i)
+        for (int i = 0; i < n; i++)
             dp[i][i] = 1;
         
-        for (int k = 1; k < n; ++k) 
+        for (int k = 1; k < n; k++) 
         {
-            for (int i = 0; i < n - k; ++i) {
+            for (int i = 0; i < n - k; i++) 
+            {
                 int j = i + k;
-                if (s[i] == s[j]) {
+                if (s[i] == s[j])
                     dp[i][j] = dp[i][j - 1];
-                } 
+                
                 else {
                     dp[i][j] = INT_MAX;
-                    for (int l = i; l < j; ++l) {
+                    for (int l = i; l < j; ++l)
                         dp[i][j] = min(dp[i][j], dp[i][l] + dp[l + 1][j]);
-                    }
                 }
             }
         }
