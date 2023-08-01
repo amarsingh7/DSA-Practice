640ae98bfd58f821d5396bcbb49bce1c9d77becc
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-08-01 12:09:23 +0530

    Time: 192 ms (99.20%), Space: 19 MB (88.41%) - LeetHub

--- 2741-special-permutations/2741-special-permutations.cpp
+++ 2741-special-permutations/2741-special-permutations.cpp
@@ -1,34 +1,21 @@
 class Solution {
 public:
-    
-    vector<vector<int>> dp;
-    int mod= 1e9+7;
-    int fck(vector<int> &nums, int pindex, int cindex, int musk)
-    {
-        if(cindex==nums.size())
-            return 1;
-        if(dp[pindex+1][musk] != -1)
-            return dp[pindex+1][musk];
-        int res=0;
-        for(int j=0; j<nums.size(); j++)
-        {
-            if(musk & (1<<j))
-                continue;
-            if(pindex==-1 || nums[pindex]% nums[j]==0 || nums[j]% nums[pindex]==0)
-            {
-                res+= fck(nums, j, cindex+1, musk | (1<<j));
-                res %= mod;
-            }  
-                
+    int dp[14][16383];
+int dfs(int i, int mask, vector<int>& nums) {
+    if (mask == (1 << nums.size()) - 1)
+        return 1;
+    if (dp[i][mask] == -1) {
+        dp[i][mask] = 0;
+        for (int j = 0; j < nums.size(); ++j) {
+            if ((mask & (1 << j)) == 0 && (mask == 0 || nums[i] % nums[j] == 0 || nums[j] % nums[i] == 0)) {
+                dp[i][mask] = (dp[i][mask] + dfs(j, mask + (1 << j), nums)) % 1000000007;
+            }
         }
-        dp[pindex+1][musk]= res;
-        return dp[pindex+1][musk];
     }
-    
-    int specialPerm(vector<int>& nums) {
-        
-        dp.resize(20, vector<int>((1<<14)+5 , -1));
-        return fck(nums, -1, 0, 0);
-        
-    }
+    return dp[i][mask];
+}
+int specialPerm(vector<int>& nums) {
+    memset(dp, -1, sizeof(dp));
+    return dfs(0, 0, nums);
+}
 };
