0e45a4f2c17e21966f1c4f7cee09fd27e1a21939
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-08-02 02:57:50 +0530

    Time: 78 ms (35.03%), Space: 7.6 MB (99.66%) - LeetHub

--- 1478-allocate-mailboxes/1478-allocate-mailboxes.cpp
+++ 1478-allocate-mailboxes/1478-allocate-mailboxes.cpp
@@ -1,35 +1,48 @@
 class Solution {
 public:
     int dp[101][101];
+
     int solve(vector<int>& houses, int k, int cur, int curk) {
+        // Base case: All houses are processed
         if (cur == houses.size()) {
+            // If required number of mailboxes is reached, return 0 (target achieved)
             if (curk == k)
                 return 0;
+            // If required number of mailboxes is not reached, return infinity (not possible)
             return INT_MAX;
         }
+        
         if (curk == k)
             return INT_MAX;
+        
         if (dp[cur][curk] != -1)
             return dp[cur][curk];
 
         long long ans = INT_MAX;
         for (int i = cur; i < houses.size(); i++) {
             long long dist = 0;
+            // getting median of current subarray
             int medi = houses[(i + cur) / 2];
+            // Calculate the total cost to allocate a mailbox in the current subarray
             for (int j = cur; j <= i; j++) {
                 dist += abs(medi - houses[j]);
             }
-            ans = min(ans, (long long)solve(houses, k, i + 1, curk + 1) + dist); 
+            // Find the minimum cost by recursively exploring the next subarray
+            ans = min(ans, (long long)solve(houses, k, i + 1, curk + 1) + dist);
         }
+       
         dp[cur][curk] = ans;
         return ans;
     }
+
+    // Main function to find the minimum distance to allocate mailboxes
     int minDistance(vector<int>& houses, int k) {
         int n = houses.size();
         sort(houses.begin(), houses.end());
 
         memset(dp, -1, sizeof(dp));
-
+        
+        // start with cur=0 in houses && curk=0 used mailboxes.
         return solve(houses, k, 0, 0);
     }
 };
