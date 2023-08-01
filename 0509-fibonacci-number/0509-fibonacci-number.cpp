04d7ab2235a331b5b706327729341267044409ce
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-08-01 14:36:02 +0530

    Time: 2 ms (69.06%), Space: 5.9 MB (90.83%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n==0) return 0;
-        if(n==1 || n==2) return 1;
-        return fib(n-1)+fib(n-2);
+        long long dp[32];
+        dp[0]= dp[1]=1;
         
-        
+        if(n==0)
+            return 0;
+        for(int i=2; i<32; i++)
+            dp[i]= dp[i-1]+dp[i-2];
+        return dp[n-1];
     }
 };
