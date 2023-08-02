a9053ef0724360ce1dbb6e90e36f7d2097fe0c33
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-08-02 20:42:53 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (38.79%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n==INT_MIN)
+        if(n < 0)  // -2147483648 
             return false;
         return bitset<32>(n).count() ==1? true:false;
     }
