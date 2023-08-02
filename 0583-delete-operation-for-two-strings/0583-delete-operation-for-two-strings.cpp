68469162d6bebc398e7970da396604eae6323411
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-08-02 15:55:37 +0530

    Time: 38 ms (18.86%), Space: 7.4 MB (88.13%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -6,10 +6,10 @@ public:
         int n= b.size();
         int i=0, j=0;
         
-        if(a.find(b) != string::npos)
-            return m-n;
-        if(b.find(a) !=string::npos)
-            return n-m;
+//         if(a.find(b) != string::npos)
+//             return m-n;
+//         if(b.find(a) !=string::npos)
+//             return n-m;
         
         for(int i=1; i<=m; i++)
         {
