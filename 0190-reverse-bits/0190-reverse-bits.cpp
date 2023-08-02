cf5d4d2aea73b2a29608c333850f99fd3a3913de
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-08-02 18:46:35 +0530

    Time: 0 ms (100.00%), Space: 6 MB (22.88%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
-        uint32_t result= 0;
-        for(int i=0; i<32; i++)
-            result = (result<<1) + (n>>i &1);
-        
-        return result;
+        uint32_t ans=0;
+        for(int i = 0; i < 32; i++)
+        {
+           //for inversing a decimal number, simply replace 2 by 10
+            ans = ans * 2 + n % 2;
+            n /= 2;
+        }
+        return ans;
     }
 };
