92639fbe467051f8b0de777be8c5cd9708fea569
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-08-03 15:24:33 +0530

    Time: 9 ms (73.06%), Space: 10 MB (41.03%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        int xorfs=0, mask=1, first=0;
-        for(auto &x:nums)
+        int xorfs = 0;  // Variable to store XOR of both 1st and 2nd numbers
+        int mask = 1;   // Mask to find the rightmost set bit in XOR result
+        int first = 0;  // Variable to store the first single number
+
+        // Step 1: Calculate XOR of all elements in 'nums' vector
+        for (auto &x : nums) {
             xorfs ^= x;
-        
-        while(!(xorfs & mask))
-            mask <<= 1;
-        
-        for(auto &x:nums)
-        {
-            if(x & mask)
-                first ^= x;
         }
-        return {first, xorfs^first};
+
+        // Step 2: Find the rightmost set bit in 'xorfs' using 'mask'
+        while (!(xorfs & mask)) {
+            mask <<= 1;
+        }
+
+        // Step 3: Find the first single number by iterating through 'nums'
+        for (auto &x : nums) {
+            if (x & mask) {
+                first ^= x;
+            }
+        }
+
+        // Step 4: Find the other single number using XOR operation
+        int second = xorfs ^ first;
+
+        // Return the vector containing both single numbers
+        return {first, second};
         
     }
 };
