054fd2837769b2b3204df4f0b7679caa6ce8ce4a
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-08-05 01:20:32 +0530

    Time: 147 ms (5.02%), Space: 74.5 MB (5.04%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -3,7 +3,8 @@ class Solution {
 public:
 bool wordBreak(string s, vector<string>& wordDict) {
     int n = s.size();
-    vector<bool> dp(n + 1);
+    bool dp[1001];
+    fill(dp, dp + n + 1, false);
     dp[n] = true;
 
     for (int i = n - 1; i >= 0; i--) {
