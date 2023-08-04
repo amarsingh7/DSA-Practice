9fe1dc94d1994f95ca46292a55774b62168a5229
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-08-05 03:05:24 +0530

    Added solution - LeetHub

--- Copy Set Bits in Range - GFG/copy-set-bits-in-range.cpp
+++ Copy Set Bits in Range - GFG/copy-set-bits-in-range.cpp
@@ -6,22 +6,35 @@ using namespace std;
 // } Driver Code Ends
 // User function Template for C++
 class Solution{
-    public:
-    int setSetBit(int x, int y, int l, int r){
-        // code here
-        bitset<32> bx(x);
-        bitset<32> by(y);
-        
-        for(int i= l-1; i<r; i++)
-        {
-            if(by[i]==1)
-                bx[i]=1;
-        }
-        bitset<32> num(bx);
-        unsigned long ans= num.to_ulong();
-        
-        return ans;
-    }
+public:
+// Function to set a range of bits in integer 'x' based on the bits in integer 'y'.
+// The range of bits to be set is from the 'l'-th bit to the 'r'-th bit (both inclusive).
+// Function to set a range of bits in integer 'x' based on the bits in integer 'y'.
+// The range of bits to be set is from the 'l'-th bit to the 'r'-th bit (both inclusive).
+int setSetBit(int x, int y, int l, int r) {
+    // Step 1: Create a mask to isolate the bits to be set in 'x'.
+    int mask = 1; // Initialize a mask with the value 1 (binary: 0000...0001).
+    
+    // Shift the mask to the left by 'r - l + 1' positions and subtract 1 to create a bit pattern
+    // with 'r - l + 1' set bits. Example: if 'l = 2' and 'r = 4', the mask will be '0000...0111'.
+    mask = (mask << (r - l + 1)) - 1;
+    
+    // Step 2: Position the mask at the 'l'-th bit in 'y' and combine it with 'y'.
+    // This operation isolates the 'r - l + 1' bits in 'y' that need to be set in 'x'.
+    // Example: if 'y = 1010110', 'l = 2', and 'r = 4', the mask will be '0000...0111' and
+    // the result will be '000000010' (the bits 2 to 4 in 'y').
+    mask = (mask << (l - 1)) & y;
+    
+    // Step 3: Set the isolated bits from 'y' in 'x'.
+    // Perform bitwise OR operation between 'x' and the mask.
+    // Example: if 'x = 1101001' and the isolated bits from 'y' are '000000010',
+    // the result of the OR operation will be '1101001 | 000000010 = 1101101'.
+    x = x | mask;
+    
+    // Step 4: Return the updated value of 'x' after setting the specified bits from 'y'.
+    return x;
+}
+
 };
 
 //{ Driver Code Starts.
