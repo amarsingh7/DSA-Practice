f10a227c54df03a5a5cd55141e2f0503d1b932fd
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-08-05 19:00:06 +0530

    Time: 4 ms (22.59%), Space: 7.1 MB (5.55%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    vector<string> letterCombinations(string digits) {
+    vector<string> ans;
+    void backtracking(string temp, string digits, int index)
+    {
         vector<string> vec= {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        if(index==digits.size())
+        {
+            ans.push_back(temp);
+            return;
+        }
+        string s= vec[digits[index]-'0'];
+        for(int i=0; i<s.size();i++)
+            backtracking(temp+s[i], digits, index+1);
+        
+    }
+    vector<string> letterCombinations(string digits) {
+        
         if(digits.empty())
             return {};
-        vector<string> ans;
-        ans.push_back("");
-        
-        int n= digits.size();
-        for(auto digit:digits)
-        {
-            vector<string> temp;
-            for(auto x:vec[digit-'0'])
-            {
-                for(auto s:ans)
-                temp.push_back(s+x);
-            }
-            ans.swap(temp);
-        }
+        backtracking("", digits, 0);
         return ans;
         
     }
