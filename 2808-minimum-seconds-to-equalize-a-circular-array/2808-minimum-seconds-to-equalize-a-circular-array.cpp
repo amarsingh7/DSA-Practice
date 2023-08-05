fd7d6ac81e75c912c3accc6aab3a6c7aa8530bd1
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-08-06 01:54:03 +0530

    Time: 585 ms (57.14%), Space: 174.4 MB (14.29%) - LeetHub

--- 2808-minimum-seconds-to-equalize-a-circular-array/2808-minimum-seconds-to-equalize-a-circular-array.cpp
+++ 2808-minimum-seconds-to-equalize-a-circular-array/2808-minimum-seconds-to-equalize-a-circular-array.cpp
@@ -1,29 +1,44 @@
 class Solution {
 public:
     int minimumSeconds(vector<int>& nums) {
-        int n=nums.size();
-        if(n==1)
-            return 0;
-        set<int> st(nums.begin(), nums.end());
-        if(n>1 and st.size()==1)
-            return 0;
+    int n = nums.size();
+    
+    // Base case: If the vector contains only one element, no swaps needed, return 0 seconds
+    if (n == 1)
+        return 0;
+    
+    // Create a set to check if all elements are the same in the vector
+    set<int> st(nums.begin(), nums.end());
+    
+    // If all elements are the same, return 0 seconds as no swaps needed
+    if (n > 1 && st.size() == 1)
+        return 0;
+
     int ans = INT_MAX;
     unordered_map<int, int> lastSeen, maxDiff;
-    for(int i = 0; i < n; ++i)
-    {
-        if(lastSeen.find(nums[i]) != lastSeen.end()){
+
+    // Loop through the vector and calculate the maximum difference between current index and last seen index for each element
+    for (int i = 0; i < n; ++i) {
+        if (lastSeen.find(nums[i]) != lastSeen.end()) {
             maxDiff[nums[i]] = max(maxDiff[nums[i]], i - lastSeen[nums[i]] - 1);
         }
         lastSeen[nums[i]] = i;
     }
-    for(int i = 0; i < n; ++i)
-    { //to handle rotation repeat the same once again
-        maxDiff[nums[i]] = max(maxDiff[nums[i]], (i - lastSeen[nums[i]] -1 + n )%n );
+
+    // Handle the case of rotation by repeating the process again
+    for (int i = 0; i < n; ++i) {
+        // To handle rotation, calculate maxDiff by considering elements at the beginning of the array after rotation
+        maxDiff[nums[i]] = max(maxDiff[nums[i]], (i - lastSeen[nums[i]] - 1 + n) % n);
         lastSeen[nums[i]] = i;
     }
-    
-    for(auto [k, v]: maxDiff) 
+
+    // Find the minimum value from maxDiff map which represents the minimum number of swaps required
+    for (auto [k, v] : maxDiff) {
         ans = min(ans, v);
-    return (ans + 1)/2; 
     }
+
+    // Return half of the minimum swaps as each swap takes two seconds
+    return (ans + 1) / 2;
+}
+
 };
