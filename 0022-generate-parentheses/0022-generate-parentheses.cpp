ee445f39903db77dc519c97cf0b4a684021d158f
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-08-05 20:31:50 +0530

    Time: 4 ms (64.14%), Space: 15.1 MB (29.91%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,31 +1,23 @@
 class Solution {
 public:
-    void ans(vector<string> &vec, int op, int cl, string s)
+    vector<string> ans;
+    void solve(int open, int close, int n, string s)
     {
-        if(op==0 and cl==0)
+        if(s.size()== 2*n)
         {
-            vec.push_back(s);
+            ans.push_back(s);
             return;
         }
-            
-        if(op!=0)
-        {
-            string opf= s+'(';
-            ans(vec, op-1, cl, opf);
-        }
-        if(op<cl)
-        {
-            string ops= s+')';
-            ans(vec, op, cl-1, ops);
-        }
+        if(open<n)
+            solve(open+1, close, n, s+"(");
+        if(close<open)
+            solve(open, close+1, n, s+")");
+        
     }
     vector<string> generateParenthesis(int n) {
-        int op=n;
-        int cl=n;
-        string s= "";
-        vector<string> vec;
-        ans(vec, op, cl, s);
+        string s="";
+        solve(0, 0, n, s);
         
-        return vec;
+        return ans;
     }
 };
