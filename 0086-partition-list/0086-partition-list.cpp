169023410df8f788afe6009b6a4bc086b382fe76
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-08-16 00:30:31 +0530

    Time: 8 ms (38.42%), Space: 10.4 MB (21.22%) - LeetHub

--- 0086-partition-list/0086-partition-list.cpp
+++ 0086-partition-list/0086-partition-list.cpp
@@ -11,21 +11,32 @@
 class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
-        ListNode node1(0), node2(0);
-        ListNode* p1= &node1;
-        ListNode* p2= &node2;
+        // Create two dummy nodes to hold the partitions
+        ListNode node1(0); // For nodes with values less than x
+        ListNode node2(0); // For nodes with values greater than or equal to x
         
-        while(head)
-        {
-            if(head->val <x)
-                p1= p1->next= head;
-            else
-                p2= p2->next= head;
-            head= head->next;
+        ListNode* p1 = &node1; // Pointer to the end of the first partition
+        ListNode* p2 = &node2; // Pointer to the end of the second partition
+        
+        // Traverse the original linked list
+        while (head) {
+            if (head->val < x) {
+                // Append the current node to the first partition
+                p1 = p1->next = head;
+            } else {
+                // Append the current node to the second partition
+                p2 = p2->next = head;
+            }
+            head = head->next;
         }
-        p2->next= NULL;
-        p1->next= node2.next;
-        return node1.next;
         
+        // Close the second partition
+        p2->next = NULL;
+        
+        // Combine the two partitions by connecting the end of the first partition to the start of the second partition
+        p1->next = node2.next;
+        
+        // Return the updated linked list with the desired partition
+        return node1.next;
     }
 };
