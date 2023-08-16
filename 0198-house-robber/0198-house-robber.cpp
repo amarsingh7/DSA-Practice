5d3dd2a8d914e604984cac0f34570ec713eba935
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-08-16 12:14:07 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (45.03%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -5,9 +5,9 @@ public:
         vector<int> dp(n+2, 0);
         for(int i=n-1; i>=0; i--)
         {
-            int skip= dp[i+1];
-            int take= nums[i]+ dp[i+2];
-            dp[i]= max(skip, take);
+            int skip= dp[i+1];  // skipping cur index
+            int take= nums[i]+ dp[i+2]; // taking the cur index
+            dp[i]= max(skip, take);  // storing the max
         }
         return dp[0];
     }
