fe61739466a8a973cc7b09526b8fd0892293a2c6
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-08-16 14:58:54 +0530

    Time: 7 ms (96.05%), Space: 22.1 MB (82.65%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.cpp
+++ 0337-house-robber-iii/0337-house-robber-iii.cpp
@@ -11,18 +11,28 @@
  */
 class Solution {
 public:
-    
-    pair<int, int> res(TreeNode* root)
-    {
-        if(root==NULL)
-            return {0, 0};
-        pair<int, int> l=  res(root->left);
-        pair<int, int> r=  res(root->right);
-        return {max(root->val + l.second +r.second, l.first +r.first), l.first +r.first};
-            
+    // A pair of integers represents the result of robbing the current subtree:
+    // First value: Maximum amount that can be robbed from the current subtree.
+    // Second value: Maximum amount that can be robbed from the subtree without including the current node.
+    pair<int, int> res(TreeNode* root) {
+        if (root == NULL)
+            return {0, 0}; 
+
+        // Recursively calculate the results for the left and right subtrees.
+        pair<int, int> l = res(root->left);
+        pair<int, int> r = res(root->right);
+
+        // Calculate the maximum amount that can be robbed from the current subtree.
+        // Case 1: Rob the current node and skip the immediate children.
+        // Case 2: Skip the current node and consider the maximum amounts from the left and right subtrees.
+        int maxRobWithCurrent =  l.first+r.first;
+        int maxRobWithoutCurrent = max(l.first+r.first, root->val + l.second + r.second);
+
+        return {maxRobWithoutCurrent, maxRobWithCurrent};
     }
+
     int rob(TreeNode* root) {
-        
+        // Return the maximum amount that can be robbed from the root node's subtree.
         return res(root).first;
     }
 };
