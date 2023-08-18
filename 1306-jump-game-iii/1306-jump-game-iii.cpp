8d16249a4a13985b46afbb60b962c1d1d8d4711a
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-08-18 18:23:31 +0530

    Time: 25 ms (98.23%), Space: 36 MB (31.35%) - LeetHub

---
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool visited[500001]{};
+    bool canReach(vector<int>& arr, int start) {
+        int n= arr.size();
+        
+        if(start<0 || start>=n || arr[start]<0 || visited[start])
+            return false;
+        if(arr[start]==0)
+            return true;
+        visited[start]= true;
+        
+        return canReach(arr, start+arr[start]) || canReach(arr, start-arr[start]);
+        
+        
+    }
+};
