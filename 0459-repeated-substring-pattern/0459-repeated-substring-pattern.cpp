10a56d198924030d8b6a520a18868d9802524282
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-08-22 02:25:30 +0530

    Time: 15 ms (83.61%), Space: 13.5 MB (57.49%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -3,22 +3,9 @@ public:
     bool repeatedSubstringPattern(string s) {
         int n= s.size();
         
-        if(n==1)
-            return false;
-        if(n==2 and s[0] !=s[1])
-            return false;
-        
-        for(int i=1; i<=n/2; i++)
-        {
-            if (n % i == 0) {
-                string pattern = "";
-                for (int j = 0; j < n / i; j++)
-                    pattern += s.substr(0, i);
-                
-                if (s == pattern) 
-                    return true;
-            }
-        }
+        string t = s + s;
+        if (t.substr(1, t.size() - 2).find(s) != -1) 
+            return true;
         return false;
     }
 };
