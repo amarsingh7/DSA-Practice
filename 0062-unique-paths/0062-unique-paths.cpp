3122cd2f6224ce680deebb2f4378245c85e65ff6
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-09-04 01:08:18 +0530

    Time: 2 ms (44.35%), Space: 6.2 MB (71.41%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,13 +1,22 @@
 class Solution {
 public:
-    
+    int dp[101][101];
+    int ans(int i, int j, int n, int m)
+    {
+        if(i == (n-1) && j == (m-1))
+            return 1;
+        if(i >=n || j >=m) 
+            return 0;
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        int left= ans(i+1,j,n,m);
+        int right= ans(i,j+1,n,m);
+        dp[i][j]= left+right;
+        
+        return dp[i][j];
+    }
     int uniquePaths(int m, int n) {
-            int N = n + m - 2;
-            int r = m - 1; 
-            double res = 1;
-            
-            for (int i = 1; i <= r; i++)
-                res = res * (N - r + i) / i;
-            return (int)res;
+        memset(dp, -1, sizeof(dp));
+        return ans(0, 0, m, n);
     }
 };
