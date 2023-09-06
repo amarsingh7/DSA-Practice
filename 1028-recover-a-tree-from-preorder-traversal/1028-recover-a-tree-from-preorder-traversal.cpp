2712199b5027768a2f42772def993f9f11c1c47c
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-09-06 19:32:29 +0530

    Time: 10 ms (96.93%), Space: 20.6 MB (32.25%) - LeetHub

--- 1028-recover-a-tree-from-preorder-traversal/1028-recover-a-tree-from-preorder-traversal.cpp
+++ 1028-recover-a-tree-from-preorder-traversal/1028-recover-a-tree-from-preorder-traversal.cpp
@@ -1,45 +1,46 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-   
-    int i=0;
-    TreeNode* solve(string &temp, int index)
-    {
-        if(index>= temp.size())
+    int i = 0; // Index to keep track of the position in the input string
+
+    TreeNode* solve(string &temp, int index) {
+        // Base case: If the current index exceeds the string length, return NULL
+        if (index >= temp.size())
             return NULL;
-        string ans="";
-        int x=0;
-        int st= i;
-        while(temp[i]=='-')
+
+        string ans = "";
+        int x = 0;
+        int st = i; // Start position for backtracking
+
+        // Count the number of hyphens to determine the level
+        while (temp[i] == '-')
             i++, x++;
-        if(x!=index){
-            i= st; return NULL;
+
+        // If the number of hyphens doesn't match the expected level, backtrack and return NULL
+        if (x != index) {
+            i = st; // Backtrack the index
+            return NULL;
         }
-            
-        while(temp[i]!= '-' and i<temp.size()){
-            ans+= temp[i]; i++;
+
+        // Extract the node value
+        while (temp[i] != '-' && i < temp.size()) {
+            ans += temp[i];
+            i++;
         }
-            
-        int val= stoi(ans);
-        
-        TreeNode* res= new TreeNode(val);
-        res->left= solve(temp, index+1);
-        res->right= solve(temp, index+1);
-        
+
+        int val = stoi(ans);
+
+        // Create a new TreeNode with the extracted value
+        TreeNode* res = new TreeNode(val);
+
+        // Recursively build the left and right subtrees
+        res->left = solve(temp, index + 1);
+        res->right = solve(temp, index + 1);
+
         return res;
     }
+
     TreeNode* recoverFromPreorder(string traversal) {
-        
+        // Call the solve function to construct the binary tree
         return solve(traversal, 0);
     }
 };
