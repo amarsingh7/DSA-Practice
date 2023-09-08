8c9c2a9305ee88f096ad17b14674f32457c29536
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-09-08 11:30:20 +0530

    Time: 3 ms (91.37%), Space: 9.7 MB (90.10%) - LeetHub

--- 1255-maximum-score-words-formed-by-letters/1255-maximum-score-words-formed-by-letters.cpp
+++ 1255-maximum-score-words-formed-by-letters/1255-maximum-score-words-formed-by-letters.cpp
@@ -1,38 +1,45 @@
 class Solution {
 public:
-    int ans = 0; // Initialize the answer variable to store the maximum score.
+    int ans = 0; 
 
     // Backtracking function to explore different combinations of words.
     void backtrack(vector<string> &words, vector<int> &score, vector<int> &count, vector<int> &used, int ind, int res) {
         // Check if any used character count exceeds its given count.
         for (int i = 0; i < 26; i++) {
             if (used[i] > count[i])
-                return; // If any character count is exceeded, stop exploring this path.
+                return; 
+            // If any character count is exceeded, stop exploring 
         }
         
-        ans = max(ans, res); // Update the maximum score if the current result is greater.
+        ans = max(ans, res); 
 
         for (int i = ind; i < words.size(); i++) {
             for (auto x : words[i]) {
-                used[x - 'a']++; // Mark characters from the current word as used.
-                res += score[x - 'a']; // Add the score of the used characters to the result.
+                used[x - 'a']++; 
+                // Mark characters from the current word as used.
+                res += score[x - 'a']; 
+                // Add score of the used characters to the result.
             }
 
-            // Recursively explore the next word while updating counts and result.
+     // Recursively explore next word while updating counts, result.
             backtrack(words, score, count, used, i + 1, res);
 
             for (auto x : words[i]) {
-                used[x - 'a']--; // Unmark characters as used (backtrack).
-                res -= score[x - 'a']; // Subtract the score of the characters from the result.
+                used[x - 'a']--; 
+                // Unmark characters as used (backtrack).
+                res -= score[x - 'a']; 
+                // Subtract score of the characters from the result.
             }
         }
     }
 
     int maxScoreWords(vector<string> &words, vector<char> &letters, vector<int> &score) {
-        vector<int> count(26, 0); // Initialize a count array to store the available character counts.
-        vector<int> used(26, 0); // Initialize an array to keep track of used character counts.
+        vector<int> count(26, 0); 
+        // Initialize a count array to store available char counts.
+        vector<int> used(26, 0); 
+        // Initialize an array to keep track of used char\ counts.
 
-        // Populate the count array with the counts of available characters.
+        // Populate the count array with counts of available char
         for (auto x : letters)
             count[x - 'a']++;
 
