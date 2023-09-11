080bd8f3aa76764071a6ee7d9201780332fa4776
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-09-11 09:37:52 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (71.97%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -5,7 +5,7 @@ public:
     int countOrders(int n) {
         long res = 1, mod = 1e9 + 7;
         for (int i = 1; i <= n; ++i)
-            res = res * (i * 2 - 1) * i % mod;
+            res = (res * (i * 2 - 1) * i) % mod;
         return res;
     }
 };
