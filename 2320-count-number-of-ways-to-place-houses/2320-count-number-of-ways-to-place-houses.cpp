4e6a12912bc4dc8db86bc1d71cc922539c518864
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-09-11 11:21:52 +0530

    Time: 17 ms (39.27%), Space: 13.5 MB (30.26%) - LeetHub

---
+++ 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    long long mod= 1e9+7;
+    long long helper( int n, vector<long long> &dp){
+        if(n == 1) return dp[n] = 2;  
+        if(n == 2) return dp[n] =  3;       
+        if(dp[n] != -1) return dp[n];
+
+
+        long long pick, npick;
+
+        pick = helper(n-2,dp)%mod;
+        npick = helper(n-1,dp)%mod;
+
+        return dp[n] = (pick+npick)%mod;
+    } 
+    int countHousePlacements(int N) {
+         vector<long long> dp(N+1,-1);
+         helper(N, dp);
+        
+         return (dp[N]*dp[N])%mod;    
+    }
+};
