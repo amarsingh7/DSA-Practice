3bc0293862ac885fbaa84f2d565e9d7cd749b713
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-09-14 20:13:29 +0530

    Time: 19 ms (72.51%), Space: 16 MB (18.19%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.cpp
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.cpp
@@ -16,7 +16,8 @@ public:
         ans.emplace_back(s);
     }          
     vector<string> findItinerary(vector<vector<string>>& tickets) {
-        
+        // storing the directed edge wala graph in ump.
+        // to make sure the lexico. order, used priority queue
         for(auto x:tickets)
         {
             string s= x[0];
@@ -24,7 +25,7 @@ public:
             
             ump[s].push(d);
         }
-        dfs("JFK");
+        dfs("JFK"); 
         reverse(ans.begin(), ans.end());
         
         return ans;
