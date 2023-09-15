15be8b8c670709cadc3df56e883e3178d0f13a1d
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-09-15 13:17:07 +0530

    Time: 348 ms (68.71%), Space: 58.3 MB (62.52%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,50 +1,63 @@
 class Solution {
 public:
-    // USING DSU IN THE MINIMUM SPANNING TREE PROBLEM
+    int par[1001]; // Array to represent disjoint sets
     
-    int par[1001];
+    // Find function to find the representative of a set
     int find(int a)
     {
-        if(par[a]<0)
+        if(par[a] < 0) // If the parent is negative, it's a root node
             return a;
-        return par[a]= find(par[a]);
+        return par[a] = find(par[a]); 
+        // Path compression for efficiency
     }
+    
+    // Union function to merge two sets
     void unify(int a, int b)
     {
-        par[a]= b;
+        par[a] = b; 
+        // Make one set's representative point to the other
     }
+    
+    // Minimum Spanning Tree algorithm using Kruskal's algorithm
     int minCostConnectPoints(vector<vector<int>>& points) {
-        int n= points.size();
-        int ans=0;
+        int n = points.size(); // Number of points
+        int ans = 0; // Initialize the answer
         
-        memset(par, -1, sizeof(par));
+        memset(par, -1, sizeof(par)); 
+        // Initialize the disjoint set data structure
         
-        vector<pair<int, pair<int, int>>> vpii;
+        vector<pair<int, pair<int, int>>> vpii; 
+        // Vector to store edges with weights
         
-        for(int i=0; i<n; i++)
+        // Generate all possible edges and calculate weights
+        for(int i = 0; i < n; i++)
         {
-            for(int j=i+1; j<n; j++)
+            for(int j = i + 1; j < n; j++)
             {
-                int weights= abs(points[i][0]- points[j][0]) + abs(points[i][1]- points[j][1]);
+                // Calculate the weight as the sum of absolute differences
+                int weight = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1]);
                 
-                vpii.push_back({weights, {i, j}});
+                // Store the weight and the pair of points (edges)
+                vpii.push_back({weight, {i, j}});
             }
         }
         
+        // Sort the edges in ascending order of weight
         sort(vpii.begin(), vpii.end());
         
-        for(auto x:vpii)
+        // Iterate through the sorted edges and build the MST
+        for(auto x : vpii)
         {
-            int a= x.second.first;
-            int b= x.second.second;
+            int a = x.second.first;
+            int b = x.second.second;
             
+            // Check if adding this edge doesn't create a cycle in mst
             if(find(a) != find(b)){
-                ans+= x.first;
-                unify(find(a), find(b));
+                ans += x.first; 
+                // Add the weight of the edge to the total cost
+                unify(find(a), find(b)); // Merge the sets
             } 
-            
         }
-        return ans;
-        
+        return ans; // Return the minimum cost to connect all points
     }
 };
