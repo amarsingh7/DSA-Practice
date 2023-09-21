89a49849fca43895b0055b1fe9c99d4f54aa8e64
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-09-21 13:49:02 +0530

    Time: 193 ms (55.35%), Space: 108.6 MB (50.31%) - LeetHub

--- 2350-shortest-impossible-sequence-of-rolls/2350-shortest-impossible-sequence-of-rolls.cpp
+++ 2350-shortest-impossible-sequence-of-rolls/2350-shortest-impossible-sequence-of-rolls.cpp
@@ -1,21 +1,32 @@
 class Solution {
 public:
     int shortestSequence(vector<int>& rolls, int k) {
-        int n= rolls.size();
-        unordered_map<int, int> ump;
-        int count=0, res=0;
-        for(auto x:rolls)
-        {
-            ump[x]++;
-            if(ump[x]==1)
-                count++;
-            if(count==k)
-            {
-                res++;
-                ump.clear();
-                count=0;
+        int n = rolls.size();  
+        // Get the number of elements in the 'rolls' vector.
+        unordered_map<int, int> ump;  
+        // Create an unordered_map to store the count of each element.
+        int count = 0;  
+        //to keep track of unique elements encountered.
+        int res = 0;  
+        //to store the length of the shortest sequence.
+
+        for (auto x : rolls) {  
+            ump[x]++;  
+            // Increment count of current element in the unordered_map.
+            
+            if (ump[x] == 1)  
+            // If count of current ele becomes 1, so it's a unique ele
+                count++;  // Increment count of unique elements.
+            
+            if (count == k) {  
+                // If we have encountered 'k' unique elements.
+                res++;  // Increment result to account for this sequence.
+                ump.clear();  
+                // Clear unordered_map to start counting next sequence.
+                count = 0;  // Reset the count of unique elements.
             }
         }
-         return res+1; 
+
+        return res + 1;  // Return the total count of sequences, adding 1 to account for the last remaining sequence.
     }
 };
