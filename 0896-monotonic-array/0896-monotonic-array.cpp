dca2ad5e9f5f7c07574f6308122965c227aa3ac3
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-09-29 20:36:20 +0530

    Time: 124 ms (53.23%), Space: 96.9 MB (13.85%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.cpp
+++ 0896-monotonic-array/0896-monotonic-array.cpp
@@ -10,12 +10,10 @@ public:
         {
             if(nums[i]<=nums[i+1])
                 fcount++;
-        }
-        for(int i=0; i<nums.size()-1; i++)
-        {
             if(nums[i]>=nums[i+1])
                 bcount++;
         }
+        
         if(fcount==n-1 || bcount==n-1)
             return true;
         return false;
