8c05bbab0f9eac4df27526bb85478bb65e18a9bd
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-10-01 19:52:14 +0530

    Time: 15 ms (25.09%), Space: 12.1 MB (6.84%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -8,40 +8,51 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
 class Solution {
 public:
     
     ListNode* reverseKGroup(ListNode* head, int k) {
-        ListNode* temp= head; 
-        vector<int> ans;
+        ListNode* temp = head;  // Pointer to traverse the linked list
+        vector<int> ans;  // Vector to store values of the linked list nodes
         
-        while(temp != NULL)
-        {
+        // Extract values from the linked list and store them in the vector
+        while (temp != NULL) {
             ans.push_back(temp->val);
-            temp= temp->next;
-                
+            temp = temp->next;
         }
         
-        int n= ans.size();
+        int n = ans.size();  // Total number of elements in the vector
+        
+        // Reverse subarrays of size 'k' in the vector
         for (int i = 0; i < ans.size(); i += k) {
             int start = i;
-            int end = i + k-1;  // Calculate the end index directly
-            bool check= false;
+            int end = i + k - 1;  // Calculate the end index directly
+            
+            bool check = false;
             if (end >= ans.size()) {
-                check= true;
+                check = true;  
+                // Set check to true if the end index exceeds the vector size
             }
-            if(check)
-                break;
-            std::reverse(ans.begin() + start, ans.begin() + end+1);
+            
+            if (check)
+                break;  
+            // Break the loop if the end index exceeds the vector size
+            
+            // Reverse the subarray in the vector
+            std::reverse(ans.begin() + start, ans.begin() + end + 1);
         }
-        int i=0;
-        temp= head;
-        while(temp)
-        {
-            temp->val= ans[i]  ;
+        
+        int i = 0;
+        temp = head;  // Reset temp to the head of the linked list
+        
+        // Update the linked list with reversed values from the vector
+        while (temp) {
+            temp->val = ans[i];
             i++;
-            temp= temp->next;
+            temp = temp->next;
         }
-        return head;
+        
+        return head;  // Return the modified head of the linked list
     }
 };
