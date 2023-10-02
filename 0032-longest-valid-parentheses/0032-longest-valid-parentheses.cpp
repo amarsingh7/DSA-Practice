e41993d10626b20b87af05024d61e428ef4251f5
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-10-02 11:33:49 +0530

    Time: 6 ms (34.59%), Space: 7.6 MB (29.04%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -4,7 +4,8 @@ public:
         int n= s.size();
         int ans=0, res=0;
         stack<int> st;
-        st.push(-1);    
+        st.push(-1);  // You can imagine there is a ) at index -1 position
+        // i.e. s[-1] = ')'  
         
         for(int i=0; i<n; i++)
         {
@@ -12,9 +13,18 @@ public:
                 st.push(i);
             else
             {
-                st.pop();
+                // if there is a match (i.e. "()"), then we can pop the stack
+                // and update the max length by (i - indices.top())
+                // (because the string in (indices.top(), i] is a valid parenthese)
+                // else if there isn't a match (i.e. "))"), then we can substitute
+                // the top of the stack by the latest one
+                // To sum up, no matter "()" or "))", we all need to pop the stack
+                st.pop();   
+                // if the stack is empty, it means a "))" occurs
+                
                 if(st.empty())
-                    st.push(i);
+                    st.push(i); 
+                // a "()" occurs
                 else
                     res= max(res, i-st.top());
             } 
