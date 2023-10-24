af295064e266399c21d6aef400c5fec71919af80
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-10-24 14:23:24 +0530

    Time: 45 ms (30.26%), Space: 25.2 MB (19.18%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.cpp
+++ 0498-diagonal-traverse/0498-diagonal-traverse.cpp
@@ -7,16 +7,20 @@ public:
         vector<int> ans;
         vector<vector<int>> diagonal(r+c-1);
         
+        // making the diagonal matrix, cells having same i+j value 
+        // are in same diagonal line
         for(int i=0; i<r; i++){
             for(int j=0; j<c; j++){
                 diagonal[i+j].push_back(mat[i][j]);
             }
         }
+        // if i is odd, order of diag is same, if even, order is reversed
         for(int i=0; i<r+c-1; i++)
         {
             if(i%2==0)
                 reverse(diagonal[i].begin(), diagonal[i].end());
         }
+        // iterate over the diagonal matrix and store it in ans vector
         for(auto x:diagonal)
             for(auto y:x)
                 ans.push_back(y);
