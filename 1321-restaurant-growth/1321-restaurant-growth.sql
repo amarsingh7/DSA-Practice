f0cf4cce038829a2f5725706e6fa1b0a57f00d7e
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-11-03 20:08:21 +0530

    Time: 593 ms (97.29%), Space: 0B (100.00%) - LeetHub

---
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT   visited_on, 
+         SUM(SUM(amount)) OVER(ORDER BY visited_on ROWS BETWEEN 6 PRECEDING AND CURRENT ROW)   AS'amount',
+         ROUND(CAST(SUM(SUM(amount)) OVER(ORDER BY visited_on ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS FLOAT)/7.0 ,2) AS'average_amount' 
+FROM     Customer 
+GROUP BY visited_on
+ORDER BY visited_on
+LIMIT 1000 OFFSET 6
