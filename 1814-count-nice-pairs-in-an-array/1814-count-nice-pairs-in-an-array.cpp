5cc1e48c0c1e05255494e4525fa73e839bb97d30
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-11-22 00:32:58 +0530

    Time: 154 ms (7.02%), Space: 57.1 MB (83.57%) - LeetHub

--- 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.cpp
+++ 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.cpp
@@ -3,8 +3,11 @@ public:
     int countNicePairs(vector<int>& A) {
         int res = 0, mod = 1e9 + 7;
         unordered_map<int, int> count;
-        for (int& a : A)
-            res = (res + count[a - rev(a)]++) % mod;
+        for (int& a : A){
+            res = (res + count[a - rev(a)]) % mod;
+            count[a-rev(a)]++;
+        }
+            
         return res;
     }
     int rev(int x) {
