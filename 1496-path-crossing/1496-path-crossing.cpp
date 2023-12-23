876d2aceb0062b39e0dc5a1537b08979d270649b
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-12-23 15:54:28 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (64.64%) - LeetHub

--- 1496-path-crossing/1496-path-crossing.cpp
+++ 1496-path-crossing/1496-path-crossing.cpp
@@ -9,9 +9,9 @@ public:
         for(auto p:path)
         {
             if(p=='N')  y++;
-            else if(p=='E')  x++;
-            else if(p=='S')  y--;
-            else if(p=='W')  x--;
+            if(p=='E')  x++;
+            if(p=='S')  y--;
+            if(p=='W')  x--;
             if(sp.count({x, y}))
                 return true;
             sp.insert({x, y});
