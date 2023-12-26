bec10c8e49942bc40013dbfe176ec153aede4993
AMAR SINGH <88260268+amarsingh7@users.noreply.github.com>
2023-12-26 11:53:44 +0530

    Time: 26 ms (53.55%), Space: 7.5 MB (77.36%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     const int MOD = 1e9+7;
+    int dp[31][10000];
     int numRollsToTarget(int n, int k, int target) {
-        vector<vector<int>> dp = vector<vector<int>> (n+1, vector<int>(target+1, 0));
+        memset(dp, 0, sizeof(dp));
         dp[0][0] = 1; // only 1 ways to roll zero dice with sum = 0
         
         for (int i=1; i<=n; ++i) 
